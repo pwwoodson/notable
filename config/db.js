51f33c9eae0cdabe435a5508a8050a81fa05ab3e
pwwoodson <69821888+pwwoodson@users.noreply.github.com>
2020-08-17 16:51:47 -0500

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+	
+        url : mongo "mongodb://cluster0-shard-00-00.qsdxl.mongodb.net:27017,cluster0-shard-00-01.qsdxl.mongodb.net:27017,cluster0-shard-00-02.qsdxl.mongodb.net:27017/notable?replicaSet=atlas-1p0hd8-shard-0" --ssl --authenticationDatabase admin --username pwwoodson --password P@ssw0rd!2
+
+}
